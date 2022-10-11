ea8039ea52c9e7d009144305fa79c4f2e8e369db
MaquinaTech <115161013+MaquinaTech@users.noreply.github.com>
2022-10-11 17:55:00 +0200

    Clase Bicicleta implementada
    Definicion de metodos necesarios en Etapa y Ciclista pero sin rellenarlos

---
+++ Bicicleta.java
@@ -0,0 +1,45 @@
+
+/**
+ * Bicicletas que usarán los Ciclistas en las Etapas.
+ * 
+ * @author (Nicolás López de lerma López) 
+ * @version (11/10/2022)
+ */
+public class Bicicleta
+{
+    private String nombre;
+    private int peso;    
+
+    /**
+     * Constructor for objects of class Bicicleta
+     */
+    public Bicicleta()
+    {
+        nombre = "";
+        peso = 0;   
+    }
+
+    /**
+     * Método para calcular la velocidad de una bicicleta usada por un Ciclista en una Etapa
+     * 
+     * @param  c1 Ciclista que usa la bicicleta
+     * @param  e1 Etapa en la que se usa la bicicleta
+     * @return    Velocidad de la bicicleta 
+     */
+    public int calcularVelocidad(Ciclista c1, Etapa e1)
+    {                
+        return (c1.getHabilidad() * 100) / (peso * e1.getDificultad());
+    }
+    
+    /**
+     * Método para calcular la velocidad de una bicicleta usada por un Ciclista en una Etapa
+     * 
+     * @param  c1 Ciclista que usa la bicicleta
+     * @param  e1 Etapa en la que se usa la bicicleta
+     * @return    Velocidad de la bicicleta 
+     */
+    public int calcularTiempo(Ciclista c1, Etapa e1)
+    {                
+        return e1.getDistancia() / (calcularVelocidad(c1, e1) * 60) ;
+    }
+}
---
+++ Etapa.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Etapa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Etapa
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Etapa
+     */
+    public Etapa()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getDificultad()
+    {
+        // put your code here
+        return 1;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getDistancia()
+    {
+        // put your code here
+        return 1;
+    }
+}
