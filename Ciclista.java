f25ad5e07c70390d282ddcc5cd0c5cc6ca5de94c
MaquinaTech <115161013+MaquinaTech@users.noreply.github.com>
2022-10-26 09:19:52 +0200

    Elimino sample method de ciclista
    Elimino sample method de ciclista

--- Ciclista.java
+++ Ciclista.java
@@ -181,16 +181,4 @@ public class Ciclista
         System.out.println("El Equipo del cilcista es " + equipo);
     }
     
-    
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
