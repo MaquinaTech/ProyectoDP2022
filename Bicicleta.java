884342624909f50ba52020b4ac46853b77f8f4eb
ivaants <114694014+ivaants@users.noreply.github.com>
2022-10-28 09:23:36 +0200

    Add files via upload

--- Bicicleta.java
+++ Bicicleta.java
@@ -1,66 +1,51 @@
 
-/**
- * Bicicletas que usarán los Ciclistas en las Etapas.
- * 
- * @author (Nicolás López de lerma López) 
- * @version (11/10/2022)
- */
-public class Bicicleta
-{
-    private String nombre;
-    private int peso;    
+public class Bicicleta {
+	private String nombre;
+	private double peso;
+	
+	Bicicleta(){
+		this.nombre = "";
+		this.peso = 0.0;
+	}
 
-    /**
-     * Constructor for objects of class Bicicleta
-     */
-    public Bicicleta()
-    {
-        nombre = "";
-        peso = 0;   
-    }
-    
-    
-    /**
-     * Devuelve el nombre del bicicleta.
-     * 
-     * @return  Nombre del bicicleta.
-     */
-    public String getNombre()
-    {
-        return nombre;
-    }
-    
-    /**
-     * Devuelve el peso de la bicicleta.
-     * 
-     * @return  Peso del ciclista.
-     */
-    public int getPeso()
-    {
-        return peso;
-    }
+	public Bicicleta(String nombre, double peso) {
+		this.nombre = nombre;
+		this.peso = peso;
+	}
 
-    /**
-     * Método para calcular la velocidad de una bicicleta usada por un Ciclista en una Etapa
-     * 
-     * @param  c1 Ciclista que usa la bicicleta
-     * @param  e1 Etapa en la que se usa la bicicleta
-     * @return    Velocidad de la bicicleta 
-     */
-    public Double calcularVelocidad(Ciclista c1, Etapa e1)
-    {                
-        return (c1.getHabilidad() * 100) / (peso * e1.getDificultad());
-    }
-    
-    /**
-     * Método para calcular la velocidad de una bicicleta usada por un Ciclista en una Etapa
-     * 
-     * @param  c1 Ciclista que usa la bicicleta
-     * @param  e1 Etapa en la que se usa la bicicleta
-     * @return    Velocidad de la bicicleta 
-     */
-    public Double calcularTiempo(Ciclista c1, Etapa e1)
-    {                
-        return e1.getDistancia() / (calcularVelocidad(c1, e1) * 60) ;
-    }
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the peso
+	 */
+	public double getPeso() {
+		return peso;
+	}
+
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public String toString() {
+		Etapa e = new Etapa();
+		return "<bicicleta: " + nombre + "> <peso: " + peso + ">) " + "en etapa " + e.getNombre();
+	}
+	
+	
 }
